dd3e26b9bea6feffc173db46aefe6d7040f561fc
jon-smith <26364802+jon-smith@users.noreply.github.com>
2024-05-18 18:10:55 +0100

    function with a string input and Result<String> return

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,6 @@
 
 int main() {
   std::cout << "2 x 2 = " << rust_cxx_square(2) << "\n";
+  std::cout << "wow = " << std::string(rust_cxx_wow("cpp"))<< "\n";
   return 0;
 }
--- src/rust_lib/src/lib.rs
+++ src/rust_lib/src/lib.rs
@@ -1,10 +1,18 @@
+use anyhow::Result;
+
 #[cxx::bridge]
 mod ffi {
     extern "Rust" {
         fn rust_cxx_square(i: i32) -> i32;
+
+        fn rust_cxx_wow(lol: &str) -> Result<String>;
     }
 }
 
 pub fn rust_cxx_square(i: i32) -> i32 {
     i*i
+}
+
+pub fn rust_cxx_wow(lol: &str) -> Result<String> {
+    Ok(format!("{}. wow.", lol))
 }
