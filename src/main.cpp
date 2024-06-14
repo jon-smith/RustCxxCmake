638bed20809d1110b5422ff07866fb0e8fa0047f
jon-smith <26364802+jon-smith@users.noreply.github.com>
2024-06-14 20:25:43 +0100

    Added reqwest library and get function/call

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,6 @@
 int main() {
   std::cout << "2 x 2 = " << rust_cxx_square(2) << "\n";
   std::cout << "wow = " << std::string(rust_cxx_wow("cpp"))<< "\n";
+  std::cout << "get = " << std::string(rust_cxx_http_get("https://httpbin.org/get", "")) << "\n";
   return 0;
 }
